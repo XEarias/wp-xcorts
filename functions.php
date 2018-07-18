19e46345c5937688f7b605154c2e124758ed071b
Daniel Torres <danieljtorres94@gmail.com>
2018-07-18 01:52:25 -0300

    footer

--- functions.php
+++ functions.php
@@ -3,6 +3,7 @@
 require_once __DIR__."/includes/supports.php";
 require_once __DIR__."/includes/mods.php";
 require_once __DIR__."/includes/packages.php";
+require_once __DIR__."/includes/sidebars.php";
 require_once __DIR__."/includes/escorts.php";
 require_once __DIR__."/includes/navigation-menus.php";
 
