8798fd598e7c2d13b94fbbdb693ba28377d5c056
XEarias <xarias13@gmail.com>
2018-07-28 14:26:27 -0300

    tamaño de imagen

--- includes/supports.php
+++ includes/supports.php
@@ -1,6 +1,8 @@
 <?php
 
 add_theme_support( 'post-thumbnails' );
+set_post_thumbnail_size( 200, 340 );
+
 add_theme_support( 'custom-logo' );
 add_theme_support( 'title-tag' );
 add_theme_support( 'custom-header', [
